1e3fba439de541dcd4fc3475ef69bfffe843b0c4
Joseph Trettevik <josephtrettevik@gmail.com>
2019-08-03 19:00:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -33,10 +33,9 @@ def reduce_to_total(source_array, starting_point = 0)
 end 
 
 def reduce_to_all_true(source_array)
+  result = true
   source_array.length.times do |index|
-    if source_array[index]
-      result = true 
-    else
+    if !source_array[index]
       result = false
       return result
     end
